7a7fa6aa595d8761ff5006170b420d85868c97f4
Nathan Ostrander <ostrander_nathan@yahoo.com>
2016-08-20 10:09:05 -0500

    IFFE examples

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("main.js")
+console.log("Sedan IIFE", Sedan);
+console.log("Sedan Color", Sedan.getColor());
+console.log("Sedan type", Sedan.getType());
+
+console.log("Sedan set Occupancy", Sedan.setOccupancy(9));
+console.log("Sedan get Occupancy", Sedan.getOccupancy());
